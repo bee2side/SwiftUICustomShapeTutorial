dde130fc7809d30ae275cb4bc62e8da27f2b60b2
bee2side <bee2side@gmail.com>
2020-07-11 14:50:48 +0900

    custom shape

--- SwiftUICustomShapeTutorial/ContentView.swift
+++ SwiftUICustomShapeTutorial/ContentView.swift
@@ -8,13 +8,39 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+
+    @available(iOS 13.0, *)
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let width = rect.width
+            let height = rect.height
+            
+            path.addLines( [
+                CGPoint(x: width * 0.4, y: height),
+                CGPoint(x: width * 0.4, y: height * 0.4),
+                CGPoint(x: width * 0.2, y: height * 0.4),
+                CGPoint(x: width * 0.5, y: height * 0.1),
+                CGPoint(x: width * 0.8, y: height * 0.4),
+                CGPoint(x: width * 0.6, y: height * 0.4),
+                CGPoint(x: width * 0.6, y: height)
+            ])
+            path.closeSubpath()
+            
+        }
+    }
+}
+
 struct ContentView: View {
+    @available(iOS 13.0, *)
     var body: some View {
-        Text("Hello, World!")
+        Arrow()
+            .stroke(lineWidth: 20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @available(iOS 13.0, *)
     static var previews: some View {
         ContentView()
     }
